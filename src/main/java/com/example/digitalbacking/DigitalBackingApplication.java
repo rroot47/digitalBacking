997a4557048f010f83f536a2bc01641d54bc3eb3
rroot47 <boubousylla2@gmail.com>
2022-05-18 00:13:02 +0200

    first commit

---
+++ src/main/java/com/example/digitalbacking/DigitalBackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalbacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitalbacking/DigitalBackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalbacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
